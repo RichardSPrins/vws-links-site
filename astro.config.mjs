064e455fb892e2847b2a0cf519a3e92f520a1200
Richard Prins <richardprins@mac.mynetworksettings.com>
2026-05-18 13:14:09 -0600

    updating styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify()
+  adapter: netlify(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
